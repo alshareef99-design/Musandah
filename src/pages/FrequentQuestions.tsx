98009cbfc7bbc2cad586c992b6ce9a604b7ea073
Lsc-sa <oalshareef94@gmail.com>
2025-09-24 12:33:53 +0300

    update faq , kl coming soon

--- src/pages/FrequentQuestions.tsx
+++ src/pages/FrequentQuestions.tsx
@@ -1,25 +1,61 @@
 import { VStack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectLanguage } from "../store/slices/languageSlice";
+import { selectLanguage, selectDirection } from "../store/slices/languageSlice";
+import FaqAccordion from "@/components/faq-components/FAQAccordion";
 import { ContactBanner } from "@/components/contact_components/ContactBanner";
 
 export const FrequentQuestions = () => {
-  const lang = useSelector(selectLanguage);
+  const Lang = useSelector(selectLanguage);
+  const direction = useSelector(selectDirection);
 
+  type FaqItem = {
+    question: string;
+    answer: string;
+  };
+
+  const [faq, setFaq] = useState<FaqItem[]>([]);
+
+  useEffect(() => {
+    fetch(direction === "rtl" ? `/docs/faq-ar.txt` : `/docs/faq.txt`)
+      .then((res) => res.text())
+      .then((data) => {
+        const items = data.split(/\d+\.\s/).filter(Boolean);
+        // Splits at "1. " , "2. " etc.
+        const parsedFaqs = items.map((item) => {
+          const [question, ...answer] = item.split("\n");
+          return {
+            question: question.trim(),
+            answer: answer.join("\n").trim(),
+          };
+        });
+        setFaq(parsedFaqs);
+      });
+  }, [Lang]);
   return (
-    <VStack w={"100vw"}>
+    <VStack justify={"center"} color={"#4D7CB1"} gap="2rem" mb={"4rem"} w={"100vw"}>
       <ContactBanner
         bgPos="0% /27%"
-        img="About.jpg"
-        title={lang === "en" ? "Frequent Questions" : "الأسئلة الشائعة"}
+        img="faq.jpg"
+        title= {Lang === "en" ? "Frequent Questions" : "الأسئلة الشائعة"}
         subtitle=""
         description=""
       />
+      {/* <Text
+        fontSize={{ base: "1.25rem", lg: "1.5rem" }}
+        color={"rgba(46, 54, 81, 1)"}
+      >
+        {Lang === "en"?
+        " Whatever is on your mind... we answer it simply and clearly.":
+          "كل ما يشغلك… نجيبك عنه ببساطة ووضوح."}
+      </Text> */}
       <VStack w="100%" minH="40vh" align="center" justifyContent="center" p={8}>
         <Text fontWeight={700} fontSize={{ base: "1.5rem", md: "2rem" }} color={"rgba(46, 54, 81, 1)"}>
-          {lang === "ar" ? "قريباً" : "Coming Soon"}
+          {Lang === "ar" ? "قريباً" : "Coming Soon"}
         </Text>
       </VStack>
+
+      {/* <FaqAccordion items={faq} /> */}
     </VStack>
   );
 };
